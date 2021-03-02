eb491213c3cbf710c0cd5b4b792e5b0514390911
Jhon9191 <joaoantonio15161516@gmail.com>
2021-03-02 13:00:45 -0300

    Configuring .html and .css files

---
+++ app/index.js
@@ -0,0 +1,5 @@
+angular.module('App', [
+    'ngAnimate',
+    'ui-router',
+    'toastr'
+])
--- gulpTasks/server.js
+++ gulpTasks/server.js
@@ -1,14 +1,13 @@
-const { equals } = require('angular');
 const gulp = require('gulp');
 const watch = require('gulp-watch');
 const server = require('gulp-webserver');
 
 
 gulp.task('watch', ()=>{
-    watch('app/**/*.html', ()=>{gulp.start('app.html')});
-    watch('app/**/*.css', ()=>{gulp.start('app.css')});
-    watch('app/**/*.js', ()=>{gulp.start('app.js')});
-    watch('assets/**/*.*', ()=>{gulp.start('app.assets')} )
+    watch('app/**/*.html', ()=> gulp.start('app.html'))
+    watch('app/**/*.css', ()=> gulp.start('app.css'))
+    watch('app/**/*.js', ()=> gulp.start('app.js'))
+    watch('assets/**/*.*', ()=> gulp.start('app.assets'))
 });
 
 gulp.task('server', ['watch'],()=>{
