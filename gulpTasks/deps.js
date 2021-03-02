a21845f7bdad2cf36c2584ea4e6f8c94f0febeca
Jhon9191 <joaoantonio15161516@gmail.com>
2021-03-02 10:08:01 -0300

    Injecting dependencies into the taskd deps

--- gulpTasks/deps.js
+++ gulpTasks/deps.js
@@ -1,9 +1,15 @@
 const gulp = require('gulp');
+const uglify = require('gulp-uglify')
+const uglifyCss = require('gulp-uglifycss');
+const concat = require('gulp-concat');
+
 
 gulp.task('deps', ['deps.js', 'deps.css', 'deps.fonts']);
 
 gulp.task('deps.css', ()=>{
-
+    return gulp.src[(
+        '...'
+    )]
 });
 
 gulp.task('deps.js', ()=>{
