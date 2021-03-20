06253d3bc578f9fa1112f76e3591d132cfa5f908
Jhon9191 <joaoantonio15161516@gmail.com>
2021-03-20 16:20:20 -0300

    Adding messages to validate operation

--- app/billingCycles/billingCyclesController.js
+++ app/billingCycles/billingCyclesController.js
@@ -1,16 +1,19 @@
 (function(){
     angular.module('primeiraApp').controller('BillingCyclesCtrl',[
         '$http',
+        'msgs',
         BillingCyclesController
     ])
 
-    function BillingCyclesController($http){
+    function BillingCyclesController($http, msgs){
         const vm = this;
         vm.create = function(){
             const url = 'http://localhost/api/billingCycles'
             $http.post(url, vm.billingCycle).then(function(response){
                 vm.billingCycle={}
-                console.log("Success!")
+                msgs.addSuccess('Operação realizada com sucesso!')
+            }).catch(function(E){
+                msgs.addError('Operação não pode ser realizada!')
             })
         }
     }
