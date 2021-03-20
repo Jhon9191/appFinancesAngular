592b33381f54cddb92f2bce26f3eabd74eddd37b
Jhon9191 <joaoantonio15161516@gmail.com>
2021-03-20 16:40:15 -0300

    Refreshing the response

--- app/billingCycles/billingCyclesController.js
+++ app/billingCycles/billingCyclesController.js
@@ -7,8 +7,17 @@
 
     function BillingCyclesController($http, msgs){
         const vm = this;
+        const url = 'http://localhost/api/billingCycles'
+
+        vm.refresh = function(){
+            $http.get(url).then(function(response){
+                vm.billingCycle = {}
+                vm.billingCycle = response.data
+            })
+        },
+
+
         vm.create = function(){
-            const url = 'http://localhost/api/billingCycles'
             $http.post(url, vm.billingCycle).then(function(response){
                 vm.billingCycle={}
                 msgs.addSuccess('Operação realizada com sucesso!')
