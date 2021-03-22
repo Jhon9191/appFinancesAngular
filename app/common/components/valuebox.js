c02017a0c76a10bd7b1252e719b022dc4861c76c
Jhon9191 <joaoantonio15161516@gmail.com>
2021-03-22 18:14:28 -0300

    Fixing credit list

--- app/common/components/valuebox.js
+++ app/common/components/valuebox.js
@@ -1,30 +1,32 @@
-angular.module('primeiraApp').component('valueBox', {
-    bindings: {
-        grid: '@',
-        collorClass: '@',
-        value: '@',
-        text: '@',
-        iconClass: '@',
-        gridClasses: '@'
-    },
-    controller: [
-        'gridSystem',
-        function (gridSystem) {
-            this.$onInit = () => this.gridClasses = gridSystem.toCssClasses(this.grid)
-        }
-    ],
-
-    template: `
-    <div class="{{ $ctrl.gridClasses }}">
-    <div class="small-box {{ $ctrl.collorClass }}">
-        <div class="inner">
-            <h3>{{ $ctrl.value }}</h3>
-            <p>{{ $ctrl.text }}</p>
-        </div>
-        <div class="icon">
-            <i class="fa {{ $ctrl.iconClass }}"> </i>
+(function(){
+    angular.module('primeiraApp').component('valueBox', {
+        bindings: {
+            grid: '@',
+            collorClass: '@',
+            value: '@',
+            text: '@',
+            iconClass: '@',
+            gridClasses: '@'
+        },
+        controller: [
+            'gridSystem',
+            function (gridSystem) {
+                this.$onInit = () => this.gridClasses = gridSystem.toCssClasses(this.grid)
+            }
+        ],
+    
+        template: `
+        <div class="{{ $ctrl.gridClasses }}">
+        <div class="small-box {{ $ctrl.collorClass }}">
+            <div class="inner">
+                <h3>{{ $ctrl.value }}</h3>
+                <p>{{ $ctrl.text }}</p>
+            </div>
+            <div class="icon">
+                <i class="fa {{ $ctrl.iconClass }}"> </i>
+            </div>
         </div>
     </div>
-</div>
-    `
-})
+        `
+    })
+})()
