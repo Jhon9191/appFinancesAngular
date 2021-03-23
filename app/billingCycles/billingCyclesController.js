b52d95a03dd644424d97a7ef53c7c49b6d7b22a4
Jhon9191 <joaoantonio15161516@gmail.com>
2021-03-23 10:45:08 -0300

    Methods for credit buttons

--- app/billingCycles/billingCyclesController.js
+++ app/billingCycles/billingCyclesController.js
@@ -1,63 +1,79 @@
-(function(){
-    angular.module('primeiraApp').controller('BillingCyclesCtrl',[
+(function () {
+    angular.module('primeiraApp').controller('BillingCyclesCtrl', [
         '$http',
         'msgs',
         'tabs',
         BillingCyclesController
     ])
 
-    function BillingCyclesController($http, msgs, tabs){
+    function BillingCyclesController($http, msgs, tabs) {
         const vm = this;
         const url = 'http://localhost/api/billingCycles'
 
-        vm.refresh = function(){
-            $http.get(url).then(function(response){
-                vm.billingCycle = {credits:[{},{},{}], debts:[{}]}
+        vm.refresh = function () {
+            $http.get(url).then(function (response) {
+                vm.billingCycle = { credits: [{}], debts: [{}] }
                 vm.billingCycles = response.data
                 tabs.show(vm, { tabList: true, tabCreate: true })
             })
         }
 
 
-        vm.create = function(){
-            $http.post(url, vm.billingCycles).then(function(response){
+        vm.create = function () {
+            $http.post(url, vm.billingCycles).then(function (response) {
                 vm.billingCycles = {}
                 vm.refresh();
                 msgs.addSuccess('Operação realizada com sucesso!')
-            }).catch(function(e){
+            }).catch(function (e) {
                 msgs.addError(e)
             })
         }
 
-        vm.showTabUpdate = function(billingCycle){
+        vm.showTabUpdate = function (billingCycle) {
             vm.billingCycle = billingCycle;
             tabs.show(vm, { tabUpdate: true })
         }
 
-        vm.showTabDelete = function(billingCycle){
+        vm.showTabDelete = function (billingCycle) {
             vm.billingCycle = billingCycle;
             tabs.show(vm, { tabDelete: true })
         }
 
-        vm.delete = function(){
+        vm.delete = function () {
             const deletUrl = `${url}/${vm.billingCycle._id}`
-            $http.delete(deletUrl, vm.billingCycle).then(function(response){
+            $http.delete(deletUrl, vm.billingCycle).then(function (response) {
                 vm.refresh();
                 msgs.addSuccess('Ciclo de pagamento excluido!')
-            }).catch(function(e){
+            }).catch(function (e) {
                 msgs.addError(e)
             })
         }
 
-        vm.update = function(){
+        vm.update = function () {
             const putUrl = `${url}/${vm.billingCycle._id}`
-            $http.put(putUrl, vm.billingCycle).then(function(response){
+            $http.put(putUrl, vm.billingCycle).then(function (response) {
                 vm.refresh();
                 msgs.addSuccess('Ciclo de pagamento atualizado!')
-            }).catch(function(e){
+            }).catch(function (e) {
                 msgs.addError(e)
             })
         }
+
+        vm.addCredit = function (index) { 
+            vm.billingCycle.credits.splice(index + 1, 0, {})
+        }
+
+        vm.cloneCredit = function (index, { name, value }) {
+            vm.billingCycle.credits.splice(index + 1, 0, { name, value })
+        }
+
+        vm.deleteCredit = function (index) {
+            console.log("clcique")
+            if (vm.billingCycle.credits.length > 1) {
+                vm.billingCycle.credits.splice(index , 1)
+            }
+        }
+
 
         vm.refresh();
     }
